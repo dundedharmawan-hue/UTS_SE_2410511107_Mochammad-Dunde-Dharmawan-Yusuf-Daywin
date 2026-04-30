a40b45aba574cab143521373e3154f97a67c492b
Dunde <dundedharmawan@gmail.com>
2026-04-30 20:12:49 +0700

    fix: fix bug pada AppServiceProvider.php

--- services/pengaduan-service/app/Providers/AppServiceProvider.php
+++ services/pengaduan-service/app/Providers/AppServiceProvider.php
@@ -1,12 +1,19 @@
+<?php
+
 namespace App\Providers;
 
-    use Illuminate\Support\ServiceProvider;
-    use Illuminate\Support\Facades\Schema; // Tambahkan ini
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
-    class AppServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
     {
-        public function boot(): void
-        {
-            Schema::defaultStringLength(191); // Tambahkan ini
-        }
+        //
     }
+
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+    }
+}
