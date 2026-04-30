6b5a808d2211f18d40bf25f4988f4a2cd58480ad
Dunde <dundedharmawan@gmail.com>
2026-04-30 19:56:32 +0700

    fix: import HasFactory trait di Pengaduan model

--- services/pengaduan-service/routes/api.php
+++ services/pengaduan-service/routes/api.php
@@ -2,13 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PengaduanController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-use App\Http\Controllers\PengaduanController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/pengaduan', [PengaduanController::class, 'index']);
 Route::post('/pengaduan', [PengaduanController::class, 'store']);
